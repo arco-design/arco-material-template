49e7d696a25bc2246ca217aaac0341e3e857e8b3
wangchen <wangchen.flsion@bytedance.com>
2021-11-29 15:29:05 +0800

    feat: add vue-library

---
+++ packages/arco-template-vue-library/template-for-monorepo/scripts/template/component/__demo__/basic.vue
@@ -0,0 +1,15 @@
+<template>
+  <@CONST_COMPONENT_NAME@>Demo Basic</@CONST_COMPONENT_NAME@>
+</template>
+
+<script lang="ts">
+import { @CONST_COMPONENT_NAME@ } from '@CONST_PACKAGE_NAME@';
+
+// 目前vue-live导出defineComponent存在问题，所以仅支持普通对象导出。
+export default {
+  name: 'BasicDemo',
+  components: {
+    @CONST_COMPONENT_NAME@,
+  },
+};
+</script>
