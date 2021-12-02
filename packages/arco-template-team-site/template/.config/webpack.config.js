fa64a4526e61094c760a956d35acac312f7de539
zuozhiheng <zuozhiheng@bytedance.com>
2021-12-02 17:59:05 +0800

    enhancement: [team-site] auto generate resolve.alias for webpack config

--- packages/arco-template-team-site/template/.config/webpack.config.js
+++ packages/arco-template-team-site/template/.config/webpack.config.js
@@ -1,1 +1,12 @@
-module.exports = (config) => {};
+const { tryGenerateAliasMapForWebpack } = require('@arco-design/arco-doc-site');
+
+/**
+ * @param config {import('@arco-design/arco-doc-site').WebpackConfig}
+ */
+module.exports = (config) => {
+  /**
+   * 🚀 Try to use resolve.alias to configure an alias for your material package to speed up dev.
+   * It's some thing like: { '@arco-design/my-material': '/project-root/packages/my-material/src' }
+   */
+  config.resolve.alias = { ...tryGenerateAliasMapForWebpack() };
+};
