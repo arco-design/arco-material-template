127dacf9338418b2ee03a2d2d288b62feed10bed
zuozhiheng <zuozhiheng@bytedance.com>
2022-01-27 17:35:04 +0800

    fix: [vue-component] UMD module name wrong

--- packages/arco-template-vue-component/template/.config/vite.config.js
+++ packages/arco-template-vue-component/template/.config/vite.config.js
@@ -1,10 +1,4 @@
 module.exports = function(config) {
-  for (const item of config.build.rollupOptions.output) {
-    if (item.format === 'umd') {
-      // 修改UMD格式下暴露到全局的变量名
-      item.name = 'TooltipButton';
-    }
-  }
-
+  // Custom vite config here
   return config;
 };
